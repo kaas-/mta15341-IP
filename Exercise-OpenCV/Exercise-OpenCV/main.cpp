4eb71c307b27ce0af919bd9d85f487b0bba7c319
pip997 <pipp9997@gmail.com>
2015-09-24 14:16:57 +0200

    Brightness added

--- Exercise-OpenCV/Exercise-OpenCV/main.cpp
+++ Exercise-OpenCV/Exercise-OpenCV/main.cpp
@@ -16,6 +16,23 @@ int PictureThresholds() {
 	return 0;
 }
 
+int ChangeBrightness() {
+	double alpha;
+	int beta;
+	Mat new_image = Mat::zeros(image.size(), image.type());
+	for (int y = 0; y < image.rows; y++) {
+		for (int x = 0; y < image.cols; x++) {
+			for (int c = 0; c < 3; c++) {
+				new_image.at<Vec3b>(y, x)[c] = //it takes each pixel from the the image rows and columns 
+					saturate_cast<uchar>(alpha*(image.at<Vec3b>(y, x)[c]) + beta); //saturate_cast make sure that the pixel is a valid pixel.
+				//To make it faster you could use "image.convertTo(new_image, -1, alpha, beta);" insted of the 2 lines above.
+			}
+		}
+	}
+
+}
+
+
 int main(int, char)
 {
 	getImage();
