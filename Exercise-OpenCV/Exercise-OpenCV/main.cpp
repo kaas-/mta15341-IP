4c0c2a634506fa081f34c0885c37793d51bb7f6c
Rasmus Christiansen <rall94.thekaas@gmail.com>
2015-09-24 13:10:52 +0200

    nessage

---
+++ Exercise-OpenCV/Exercise-OpenCV/main.cpp
@@ -0,0 +1,17 @@
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int getImage() {
+	for (int i = 0; i <= 5; i++)
+	{
+		cout << i;
+	}
+	return 0;
+}
+
+int main(int, char)
+{
+	return 0;
+}
